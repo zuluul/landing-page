97ebfba183bb069305bf70b8242f153118c10e58
Ilham Ardiansyah <41895896+zuluul@users.noreply.github.com>
2021-08-02 17:18:42 +0700

    init

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TEMAN KEUD354</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <script src="bootstrap/jquery/jquery-3.6.0.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h3>Sistem Pengelolaan Keuangan Desa Terintegrasi</h3>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="img/siskeudes.png" alt="Card image cap" width="200" height="100">
+            <div class="card-body">
+                <h5 class="card-title">SISKEUDES</h5>
+                <p class="card-text">Sistem Keuangan Desa</p>
+                <a href="https://siskeudes-cianjurkab.simdacloud.id/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="card border-primary" style="width: 18rem;">
+            <img class="card-img-top" src="img/sisalsa.png" alt="Card image cap" width="200" height="100">
+            <div class="card-body">
+                <h5 class="card-title">SISALSA</h5>
+                <p class="card-text">Sistem Informasi Salur Dana Desa</p>
+                <a href="http://103.81.246.152/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="img/sitanti.png" alt="Card image cap" width="200" height="100">
+            <div class="card-body">
+                <h5 class="card-title">SITANTI</h5>
+                <p class="card-text">Sistem Informasi Transaksi Non Tunai</p>
+                <a href="http://103.81.246.153/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="img/siskeudes.png" alt="Card image cap" width="200" height="100">
+            <div class="card-body">
+                <h5 class="card-title">EIS</h5>
+                <p class="card-text">Excecutive Information System</p>
+                <a href="#" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
