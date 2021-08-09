159050c7a41474e9955b7ff0a264b9844307fae4
Ilham Ardiansyah <41895896+zuluul@users.noreply.github.com>
2021-08-09 09:44:10 +0700

    new design

--- index.php
+++ index.php
@@ -4,84 +4,55 @@
   <title>TEMAN KEUD354</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <script src="bootstrap/jquery/jquery-3.6.0.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/style.css"> 
 </head>
 <body>
-
-<div class="jumbotron text-center">
-  <h2>TEMAN KEUD354</h2>
-  <h3>Sistem Pengelolaan Keuangan Desa Terintegrasi</h3>
-</div>
-  
-<div class="container">
-  <div class="row">
-    <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="img/siskeudes.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">SISKEUDES</h5>
-                <p class="card-text">Sistem Keuangan Desa</p>
-                <a href="https://siskeudes-cianjurkab.simdacloud.id/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+    <main>
+        <aside>
+            <img src="assets/img/temankeud354.svg" width="280px" height="180px"><br>
+            Sistem Pengelolaan Keuangan Desa Terintegrasi
+        </aside>   
+        <div id="content">
+            <div class="card-inside">
+                <img src="assets/img/siskeudes.svg" width="200px" height="100px"><br>
+                Sistem Keuangan Desa<br><br>
+                <a class="button" href="https://siskeudes-cianjurkab.simdacloud.id/" target="_blank">Kunjungi Situs</a>
             </div>
-        </div>
-    </div>
-    <div class="col-lg-3">
-        <div class="card border-primary" style="width: 18rem;">
-            <img class="card-img-top" src="img/sisalsa.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">SISALSA</h5>
-                <p class="card-text">Sistem Informasi Salur Dana Desa</p>
-                <a href="https://sisalsa-cianjurkab.smartvillage.info/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            <div class="card-inside">
+                <img src="assets/img/sisalsa.svg" width="200px" height="100px"><br>
+                Sistem Informasi Salur Dana Desa<br><br>
+                <a class="button" href="https://sisalsa-cianjurkab.smartvillage.info/" target="_blank">Kunjungi Situs</a>                
             </div>
-        </div>
-    </div>
-    <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="img/sitanti.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">SITANTI</h5>
-                <p class="card-text">Sistem Informasi Transaksi Non Tunai</p>
-                <a href="https://sitanti-cianjurkab.smartvillage.info/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            <div class="card-inside">
+            <img src="assets/img/sitanti.png" width="200px" height="100px"><br>
+            Sistem Informasi Transaksi Non Tunai<br><br>
+                <a class="button" href="https://sitanti-cianjurkab.smartvillage.info/" target="_blank">Kunjungi Situs</a>
+                
             </div>
-        </div>
-    </div>
-    <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="img/siskeudes.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">EIS</h5>
-                <p class="card-text">Excecutive Information System</p>
-                <a href="http://eis-cianjurkab.smartvillage.info/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            <div class="card-inside">
+            <img src="assets/img/eis.svg" width="200px" height="100px"><br>
+            Excecutive Information System<br><br>
+                <a class="button" href="http://eis-cianjurkab.smartvillage.info/" target="_blank">Kunjungi Situs</a>
+                
             </div>
-        </div>
-    </div>
-  </div>
-  <div class="row">
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="img/ibc.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">IBC-BJB</h5>
-                <p class="card-text">Internet Bank Corporate</p>
-                <a href="https://bjbcorp.bankbjb.co.id/corporate/" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            <div class="card-inside">
+            <img src="assets/img/ibc.svg" width="200px" height="100px"><br>
+            Internet Bank Corporate<br><br>
+                <a class="button" href="https://bjbcorp.bankbjb.co.id/corporate/" target="_blank">Kunjungi Situs</a>
+                
             </div>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="img/djp.png" alt="Card image cap" width="200" height="100">
-            <div class="card-body">
-                <h5 class="card-title">DJP Online</h5>
-                <p class="card-text">Direktorat Jenderal Pajak Online</p>
-                <a href="https://djponline.pajak.go.id/account/login" class="btn btn-primary" target="_blank">Kunjungi Situs</a>
+            <div class="card-inside">
+            <img src="assets/img/djp.svg" width="200px" height="100px"><br>
+            Direktorat Jenderal Pajak Online<br><br>
+                <a class="button" href="https://djponline.pajak.go.id/account/login" target="_blank">Kunjungi Situs</a>
+                
             </div>
+    
         </div>
-      </div>
-  </div>
-
-</div>
-
+        <div class="break"></div>
+        <div class="footer">
+            Copyright &copy; 2021 PT. LAWANG SEWU TEKNOLOGI
+        </div> 
+    </main>
 </body>
 </html>
